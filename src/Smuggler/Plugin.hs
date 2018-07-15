14ae6935ff4fa775007b7531d5ae4a76c08f2f65
Dmitry Kovanikov <kovanikov@gmail.com>
2018-07-15 18:09:37 +0800

    [#24] Add file hashes with hash-store

--- src/Smuggler/Plugin.hs
+++ src/Smuggler/Plugin.hs
@@ -2,8 +2,12 @@ module Smuggler.Plugin
        ( plugin
        ) where
 
+import Control.Monad (void)
 import Control.Monad.IO.Class (MonadIO (..))
+import Data.ByteString (ByteString)
+import HashStore (hashStore)
 import Language.Haskell.GHC.ExactPrint (exactPrint)
+import System.FilePath (takeFileName)
 
 import HscTypes (ModSummary (..))
 import Plugins (CommandLineOption, Plugin (..), defaultPlugin)
@@ -12,14 +16,25 @@ import TcRnTypes (TcGblEnv (..), TcM)
 import Smuggler.Anns (removeAnnAtLoc)
 import Smuggler.Parser (runParser)
 
+import qualified Data.ByteString as BS
+
 plugin :: Plugin
 plugin = defaultPlugin { typeCheckResultAction = smugglerPlugin }
+
+cacheDir :: FilePath
+cacheDir = ".smuggler"
 
 smugglerPlugin :: [CommandLineOption] -> ModSummary -> TcGblEnv -> TcM TcGblEnv
 smugglerPlugin _ modSummary tcEnv = do
     let modulePath = ms_hspp_file modSummary
 
-    (anns, ast) <- liftIO $ runParser modulePath
-    liftIO $ putStrLn $ exactPrint ast $ removeAnnAtLoc 4 31 anns
+    fileContent <- liftIO $ BS.readFile modulePath
+    liftIO $ void $ hashStore cacheDir (smuggling modulePath) (takeFileName modulePath, fileContent)  -- TODO: remove takeFileName
 
     pure tcEnv
+  where
+    smuggling :: FilePath -> ByteString -> IO ByteString
+    smuggling modulePath _ = do
+        (anns, ast) <- runParser modulePath  -- TODO: don't read file, use given ByteString
+        putStrLn $ exactPrint ast $ removeAnnAtLoc 4 31 anns
+        pure ""
