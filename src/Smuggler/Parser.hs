33f28d42372e111c66c1279e87c803afc15c5762
Kenny Shen <kenny@machinesung.com>
2018-10-04 21:13:25 +0800

    Use fileContents in parser

--- src/Smuggler/Parser.hs
+++ src/Smuggler/Parser.hs
@@ -2,15 +2,18 @@ module Smuggler.Parser
        ( runParser
        ) where
 
-import Language.Haskell.GHC.ExactPrint (Anns, parseModule)
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (unpack)
+import Language.Haskell.GHC.ExactPrint (Anns)
+import Language.Haskell.GHC.ExactPrint.Parsers (parseModuleFromString)
 
 import HsExtension (GhcPs)
 import HsSyn (HsModule (..))
 import SrcLoc (Located)
 
-runParser :: FilePath -> IO (Either () (Anns, Located (HsModule GhcPs)))
-runParser fileName = do
-    res <- parseModule fileName
+runParser :: FilePath -> ByteString -> IO (Either () (Anns, Located (HsModule GhcPs)))
+runParser fileName fileContents = do
+    res <- parseModuleFromString fileName (unpack fileContents)
     pure $ case res of
         Left (_srcSpan, _str) -> Left ()
         Right x               -> Right x
